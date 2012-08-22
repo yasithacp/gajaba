2c790c4f67e0c6d2e5f89478fd6c447901ce3ef5
Yasitha Pandithawatta <yasitha4@gmail.com>
2012-08-22 23:59:57 +0530

    Create json string from cache

--- gajaba-simulator/src/main/java/org/gajaba/simulator/Simulator.java
+++ gajaba-simulator/src/main/java/org/gajaba/simulator/Simulator.java
@@ -92,26 +92,4 @@ public class Simulator extends AbstractHandler {
             output.write(buffer, 0, bytesRead);
         }
     }
-
-    private static String readFileAsString(String filePath) throws IOException {
-
-        StringBuffer fileData = new StringBuffer(1000);
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        char[] buf = new char[1024];
-
-        int numRead = 0;
-        while ((numRead = reader.read(buf)) != -1) {
-
-            String readData = String.valueOf(buf, 0, numRead);
-
-            fileData.append(readData);
-
-            buf = new char[1024];
-
-        }
-        reader.close();
-        return fileData.toString();
-
-    }
-
 }
--- gajaba-simulator/src/main/java/org/gajaba/simulator/TreeJsonServlet.java
+++ gajaba-simulator/src/main/java/org/gajaba/simulator/TreeJsonServlet.java
@@ -1,7 +1,6 @@
 package org.gajaba.simulator;
 
 
-import com.sun.enterprise.ee.cms.core.GMSCacheable;
 import org.gajaba.group.GMSSeparator;
 import org.gajaba.server.Server;
 
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -33,151 +31,45 @@ public class TreeJsonServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
         Map map = this.server.getDistributedcache();
         String json = this.writeDataFromCache(map);
-        out.print("{\n" +
-                " \"name\": \"Load Balancer\",\n" +
-                " \"children\": [\n" +
-                "\n" +
-                " ]\n" +
-                "}");
-    }
-
-    private void feedDummyData(PrintWriter out){
-
-        out.print("{\n" +
-                " \"name\": \"Load Balancer\",\n" +
-                " \"children\": [\n" +
-                "  {\n" +
-                "   \"name\": \"Server 1\",\n" +
-                "   \"children\": [\n" +
-                "    {\n" +
-                "     \"name\": \"Load = 50\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "     \"name\": \"Services\",\n" +
-                "     \"children\": [\n" +
-                "      {\"name\": \"Service_1\", \"size\": 3534},\n" +
-                "      {\"name\": \"Service_2\", \"size\": 5731},\n" +
-                "      {\"name\": \"Service_3\", \"size\": 7840},\n" +
-                "      {\"name\": \"Service_4\", \"size\": 5914}\n" +
-                "     ]\n" +
-                "    },\n" +
-                "    {\n" +
-                "     \"name\": \"Server Type = type_1\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Server Mode = mode_1\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Server Status = Connected\"\n" +
-                "    }\n" +
-                "   ]\n" +
-                "  },\n" +
-                "  {\n" +
-                "   \"name\": \"Server 2\",\n" +
-                "   \"children\": [\n" +
-                "    {\n" +
-                "     \"name\": \"Load = 30\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "     \"name\": \"Services\",\n" +
-                "     \"children\": [\n" +
-                "      {\"name\": \"Service_1\", \"size\": 3534},\n" +
-                "      {\"name\": \"Service_2\", \"size\": 5731},\n" +
-                "      {\"name\": \"Service_4\", \"size\": 5914}\n" +
-                "     ]\n" +
-                "    },\n" +
-                "    {\n" +
-                "     \"name\": \"Server Type = type_1\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Server Mode = mode_2\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Server Status = Connected\"\n" +
-                "    }\n" +
-                "   ]\n" +
-                "  },\n" +
-                "  {\n" +
-                "   \"name\": \"Server 3\",\n" +
-                "   \"children\": [\n" +
-                "    {\n" +
-                "     \"name\": \"Load = 700\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "     \"name\": \"Services\",\n" +
-                "     \"children\": [\n" +
-                "      {\"name\": \"Service 2\", \"size\": 5731}\n" +
-                "     ]\n" +
-                "    },\n" +
-                "    {\n" +
-                "     \"name\": \"Server Type = type_2\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Server Mode = mode_1\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Server Status = Connected\"\n" +
-                "    }\n" +
-                "   ]\n" +
-                "  },\n" +
-                "  {\n" +
-                "   \"name\": \"Server 4\",\n" +
-                "   \"children\": [\n" +
-                "    {\n" +
-                "     \"name\": \"Load = 10\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "     \"name\": \"Services\",\n" +
-                "     \"children\": [\n" +
-                "      {\"name\": \"Service_1\", \"size\": 3534},\n" +
-                "      {\"name\": \"Service_2\", \"size\": 5731}\n" +
-                "     ]\n" +
-                "    },\n" +
-                "    {\n" +
-                "     \"name\": \"Server Type = type_1\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Server Mode = mode_1\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Server Status = Connected\"\n" +
-                "    }\n" +
-                "   ]\n" +
-                "  }\n" +
-                " ]\n" +
-                "}");
-    }
-
-    // method to create a dummy cache
-    private HashMap createDummyCache(){
-        HashMap<GMSCacheable, String> cache = new HashMap();
-        cache.put(new GMSCacheable("ComponentName1", "MemberTokenId1", (Serializable) "key1"), "value1");
-        cache.put(new GMSCacheable("ComponentName1", "MemberTokenId1", (Serializable) "key2"), "value2");
-        cache.put(new GMSCacheable("ComponentName1", "MemberTokenId1", (Serializable) "key3"), "value3");
-        cache.put(new GMSCacheable("ComponentName2", "MemberTokenId1", (Serializable) "key4"), "value4");
-        cache.put(new GMSCacheable("ComponentName3", "MemberTokenId1", (Serializable) "key5"), "value5");
-        cache.put(new GMSCacheable("ComponentName1", "MemberTokenId1", (Serializable) "key6"), "value6");
-        cache.put(new GMSCacheable("ComponentName1", "MemberTokenId1", (Serializable) "key7"), "value7");
-        cache.put(new GMSCacheable("ComponentName2", "MemberTokenId1", (Serializable) "key8"), "value8");
-        cache.put(new GMSCacheable("ComponentName3", "MemberTokenId1", (Serializable) "key9"), "value9");
-        cache.put(new GMSCacheable("ComponentName1", "MemberTokenId1", (Serializable) "key10"), "value10");
-        cache.put(new GMSCacheable("ComponentName3", "MemberTokenId1", (Serializable) "key11"), "value11");
-        cache.put(new GMSCacheable("ComponentName1", "MemberTokenId1", (Serializable) "key12"), "value12");
-        cache.put(new GMSCacheable("ComponentName3", "MemberTokenId1", (Serializable) "key13"), "value13");
-        cache.put(new GMSCacheable("ComponentName1", "MemberTokenId1", (Serializable) "key14"), "value14");
-        return cache;
+        out.print(json);
     }
 
     private String writeDataFromCache(Map map){
 
         HashMap<String, ArrayList<String>> stringMap = getMapAsAnArray(map);
-        String html = "";
+        String html = "{\n" +
+                " \"name\": \"Load Balancer\",\n" +
+                " \"children\": [\n" +
+                "  {";
         Iterator it = stringMap.entrySet().iterator();
+        int var = 1;
         while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry)it.next();
+            ArrayList<String> values = (ArrayList<String>) pairs.getValue();
+            html += "   \"name\": \""+ pairs.getKey()+"\",\n" +
+                    "   \"children\": [\n";
 
-
-
+            for(int i=0; i< values.size(); i++){
+                html += "    {\n" +
+                        "     \"name\": \"" + values.get(i) + "\"\n" +
+                        "    }";
+                if( i != values.size()-1){
+                    html += ",\n";
+                } else {
+                    html += "\n";
+                }
+            }
+            html += "   ]\n";
+            if(var != stringMap.size()){
+                html += "  },\n";
+            } else {
+                html += "  }\n";
+            }
+            var++;
+            it.remove();
         }
+        html += " ]\n" +
+                "}";
         return html;
     }
 
@@ -205,10 +97,4 @@ public class TreeJsonServlet extends HttpServlet {
         return stringMap;
     }
 
-//    private void temp(){
-//        HashMap<GMSCacheable, String> cache = createDummyCache();
-//        System.out.println("The raw map: "+cache);
-//        ValueComparator bvc =  new ValueComparator(map);
-//        TreeMap<String,Double> sorted_map = new TreeMap<String,Double>(bvc);
-//    }
 }
